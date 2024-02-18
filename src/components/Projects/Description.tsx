7eda40f45a0c5cd665fa7be49fa27ca890c82915
김준하 <junhakjh@gmail.com>
2024-02-18 13:52:45 +0900

    feat: 프로젝트 링크 추가

--- src/components/Projects/Description.tsx
+++ src/components/Projects/Description.tsx
@@ -1,18 +1,32 @@
+import Link from 'next/link';
+
 type Props = {
   description: string;
   period: {
     start: string;
     end?: string;
   };
+  links?: [];
 };
 
-export default function Description({ description, period }: Props) {
+export default function Description({ description, period, links }: Props) {
   return (
     <>
       <p className='md:text-xl'>{description}</p>
       <p className='md:text-lg text-base text-gray-600'>
         <time>{period.start}</time> ~ {period.end ? <time>{period.end}</time> : '진행중'}
       </p>
+      {links && (
+        <ul>
+          {links.map((link) => (
+            <li key={link}>
+              <Link className='md:text-lg text-base text-gray-600' href={link} target='_blank' rel='noreferrer'>
+                <u>{link}</u>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
--- src/model/project.ts
+++ src/model/project.ts
@@ -16,6 +16,7 @@ export type ProjectInfoType = {
     start: string;
     end?: string;
   };
+  links?: [];
   details: string[];
   images: ProjectImageType[];
   techniques: TechniqueType[];
